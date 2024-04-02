aef2bccd26e1294de0bde6ab865a2e6c3b4a415a
Ayoola <ayo.oloyede16@gmail.com>
2024-04-02 20:57:25 +0100

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const { handle_404_requests } = require("../controller/errorRouteController");
 const statusRoutes = require("./statusRoutes");
 const sampleRoutes = require("./sampleRoutes");
+const eventRoutes = require("./eventRoutes");
+const screenshotRoutes = require("./screenshotRoutes");
 const eyetrackingdataRoutes = require("./eyetrackingdataRoutes")
 
 
@@ -8,6 +10,8 @@ module.exports = (app) => {
     // declaring all the different parent paths to be used in the api
     app.use("/api/v1/status", statusRoutes);
     app.use("/api/v1/samples", sampleRoutes);
+    app.use("/api/v1/events", eventRoutes);
+    app.use("/api/v1/screenshots", screenshotRoutes);
     app.use("/api/v1/eyetrackingdata", eyetrackingdataRoutes);
 
     
