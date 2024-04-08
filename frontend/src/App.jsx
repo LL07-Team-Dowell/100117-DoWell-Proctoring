01ae364d375507a75c630fa167732c721c029af6
Blackbullet99 <isaacokanigbe@gmail.com>
2024-04-08 23:54:13 +0100

    added screen capture functionality and updated event form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,50 @@
-import { useUserContext } from './contexts'
-import LoadingPage from './pages/LoadingPage/LoadingPage'
-import useDowellLogin from './hooks/useDowellLogin'
-import { Route, Routes } from 'react-router-dom'
-import React from 'react'
-import { publicUserRoutes } from './routes/publicUserRoutes'
-import { loggedInUserRoutes } from './routes/loggedInUserRoutes'
+import { useUserContext } from "./contexts";
+import LoadingPage from "./pages/LoadingPage/LoadingPage";
+import useDowellLogin from "./hooks/useDowellLogin";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { publicUserRoutes } from "./routes/publicUserRoutes";
+import { loggedInUserRoutes } from "./routes/loggedInUserRoutes";
 
 function App() {
-  const {
-    currentUser,
-    isPublicUser,
-    userDetailLoading,
-  } = useUserContext();
+  const { currentUser, isPublicUser, userDetailLoading } = useUserContext();
 
   useDowellLogin();
 
-  if (userDetailLoading) return <Routes>
-    <Route path='*' element={<LoadingPage />} />  
-  </Routes>
+  if (userDetailLoading)
+    return (
+      <Routes>
+        <Route path="*" element={<LoadingPage />} />
+      </Routes>
+    );
 
-  if (isPublicUser) return <Routes>
-    {
-      React.Children.toArray(publicUserRoutes.map(item => {
-        return <Route 
-          path={item.route}
-          element={<item.component />}
-        />
-      }))
-    }
-  </Routes>
+  if (isPublicUser)
+    return (
+      <Routes>
+        {React.Children.toArray(
+          publicUserRoutes.map((item) => {
+            return <Route path={item.route} element={<item.component />} />;
+          })
+        )}
+      </Routes>
+    );
 
-  if (!currentUser) return <Routes>
-    <Route path='*' element={<>User detail not found</>} />
-  </Routes>
-  
-  return <Routes>
-    {
-      React.Children.toArray(loggedInUserRoutes.map(item => {
-        return <Route 
-          path={item.route}
-          element={
-            <item.component />
-          }
-        />
-      }))
-    }
-  </Routes>
+  if (!currentUser)
+    return (
+      <Routes>
+        <Route path="*" element={<>User detail not found</>} />
+      </Routes>
+    );
+
+  return (
+    <Routes>
+      {React.Children.toArray(
+        loggedInUserRoutes.map((item) => {
+          return <Route path={item.route} element={<item.component />} />;
+        })
+      )}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/utils/captureScreen.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+import html2canvas from "html2canvas";
+
+const ScreenCapture = ({ children, captureScreen }) => {
+  const captureRef = useRef(null);
+
+  const handleCapture = () => {
+    if (!captureRef.current) return;
+
+    html2canvas(captureRef.current)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        console.log(imgData);
+      })
+      .catch((error) => {
+        console.error("Error while capturing:", error);
+      });
+  };
+
+  return (
+    <>
+      <div ref={captureRef}>{children}</div>
+      {captureScreen ? <button onClick={handleCapture}>Capture</button> : null}
+    </>
+  );
+};
+
+export default ScreenCapture;
