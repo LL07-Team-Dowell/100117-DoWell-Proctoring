f67a2dd1be5c32baaaa0021d39b3a6900b2abdb6
Muhammad Ibrahim <ibrahimmuhammad271@gmail.com>
2024-04-23 03:44:29 +0100

    Completed the implementation of Face detection and conparison from two provided images

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose");
 exports.connectToDb = async () => {
     // connecting to the database
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URI_ONLINE);
         console.log('Connected to DB')
     } catch (error) {
         console.log('Failed to connect to DB: ', error);
