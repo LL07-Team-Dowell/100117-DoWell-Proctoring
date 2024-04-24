66e12d0c798f34b1d6b922c19a799707da3ae086
Manish <mdashsharma95@gmail.com>
2024-04-24 17:24:50 +0530

    fix: Vite config file to run in sever

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const localOrigin = "http://127.0.0.1:4173";
-const productionOrigin = "http://0.0.0.0:4173";
+// const localOrigin = "http://127.0.0.1:4173";
+// const productionOrigin = "http://0.0.0.0:4173";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://www.uxlive.me/dowellproctoring/',
+  // base: '/dowellproctoring/',
   plugins: [react()],
-  server: {
-    port: 4173,
-    strictPort: true,
-    host: true,
-    origin: productionOrigin,
-  },
+  // server: {
+  //   port: 4173,
+  //   strictPort: true,
+  //   host: true,
+  //   origin: productionOrigin,
+  // },
 })
