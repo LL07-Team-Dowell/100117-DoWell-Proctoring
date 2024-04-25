cac402b409e5e4d681f575b6e5c1cf660ccb37db
Manish <mdashsharma95@gmail.com>
2024-04-25 05:45:56 +0530

    fixed the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // const localOrigin = "http://127.0.0.1:4173";
-// const productionOrigin = "http://0.0.0.0:4173";
+const productionOrigin = "http://0.0.0.0:4173";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dowellproctoring/',
+  base: '/',
   plugins: [react()],
-  // server: {
-  //   port: 4173,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: productionOrigin,
-  // },
+  server: {
+    port: 4173,
+    strictPort: true,
+    host: true,
+    origin: productionOrigin,
+  },
 })
