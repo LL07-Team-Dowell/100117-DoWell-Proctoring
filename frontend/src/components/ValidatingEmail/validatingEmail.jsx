64c7213f38772eb9895c1a1cea29a7b8c0c7cd7e
Blackbullet99 <isaacokanigbe@gmail.com>
2024-04-29 11:49:12 +0100

    updated the sending of email logic

--- frontend/src/components/ValidatingEmail/validatingEmail.jsx
+++ frontend/src/components/ValidatingEmail/validatingEmail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./validateEmail.css";
@@ -17,12 +18,12 @@ const EmailInput = ({ newEvent }) => {
     }
   };
 
-  const addEmail = (email) => {
+  const addEmail = (email, name) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isValidEmail = re.test(String(email).toLowerCase());
     setEmails((prevEmails) => [
       ...prevEmails,
-      { email, isValid: isValidEmail },
+      { email, name, isValid: isValidEmail },
     ]);
     setInputValue("");
     if (!isValidEmail) {
@@ -37,22 +38,25 @@ const EmailInput = ({ newEvent }) => {
   };
 
   const handleSendInvite = () => {
-    // if (notValid) return toast.error("Correct the invalid email address");
-    // const emailAddresses = emails.map((emailObj) => emailObj.email);
+    if (notValid) return toast.error("Correct the invalid email address");
+    const emailAddresses = emails.map((emailObj) => ({
+      email: emailObj.email,
+      name: "Participant",
+    }));
 
-    // const message = "This is the message content of the email.";
-    // const email = emailAddresses.join(",");
+    const message = "This is the message content of the email.";
+    const email = emailAddresses;
 
-    // sendEmail(message, email, newEvent)
-    //   .then((response) => {
-    //     console.log("Email sent successfully:", response);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error sending email:", error);
-    //   });
-    // toast.success("Invitation sent successfully");
-    // console.log("Email addresses:", emailAddresses);
-    toast.info("Still in development");
+    sendEmail(message, email, newEvent)
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+    toast.success("Invitation sent successfully");
+    console.log("Email addresses:", email);
+    // toast.info("Still in development");
   };
 
   return (
