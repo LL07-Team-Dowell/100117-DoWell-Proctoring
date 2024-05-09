6be55932acf8674888e877972294d0e9ca52631e
Ayoola <ayo.oloyede16@gmail.com>
2024-05-09 09:04:42 +0100

    Updates

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
 
 
-
-
 // different API base URLs
 const loginBaseUrl = 'https://100014.pythonanywhere.com/api/';
 const clientAdminBaseUrl = 'https://100093.pythonanywhere.com/api/';
+// const currentBaseApiOrigin = 'http://localhost:5000'; // local
+const currentBaseApiOrigin = 'https://www.uxlive.me'; // prod
+
+
+// CONFIG FOR PEERJS (TO USE LOCALLY: comment 18-20 and uncomment 12-15)
+// // Local environment usage
+// const peerServerHost = 'localhost';
+// const peerServerPort = 9000;
+// const peerServerPath = '/myapp';
 
 // Default values for production environment
-let peerServerHost = 'uxlive.me';
-let peerServerPort = 9000;
-let peerServerPath = '/dowellproctoring/peer/myapp';
-let currentBaseApiOrigin = 'https://www.uxlive.me'; //prod
+const peerServerHost = 'uxlive.me';
+const peerServerPort = 9000;
+const peerServerPath = '/dowellproctoring/peer/myapp';
 
-if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    // CONFIG FOR PEERJS
-    // Local usage
-    peerServerHost = 'localhost';
-    peerServerPort = 9000;
-    peerServerPath = '/myapp';
-
-    // Running locally
-    currentBaseApiOrigin = 'http://localhost:5000'; //local
-}
 
 // creating separate axios instances for each API interaction
 const loginAxiosInstance = axios.create({
@@ -36,8 +32,8 @@ const clientAdminAxiosInstance = axios.create({
 })
 
 const defaultAxiosInstance = axios.create({
-    // baseURL: `${currentBaseApiOrigin}/api/v1`, // local
-    baseURL: `${currentBaseApiOrigin}/dowellproctoring-backend/api/v1/`,
+    // baseURL: `${currentBaseApiOrigin}/api/v1`, // local usage
+    baseURL: `${currentBaseApiOrigin}/dowellproctoring-backend/api/v1/`, // production usage
     withCredentials: true,
 })
 
@@ -50,5 +46,4 @@ export {
     peerServerHost,
     peerServerPort,
     peerServerPath,
-    
 }
