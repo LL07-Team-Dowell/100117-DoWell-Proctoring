f3bcff7910397be784db5cd6778e4906069dac57
Blackbullet99 <isaacokanigbe@gmail.com>
2024-05-27 17:53:00 +0100

    commited changes

--- frontend/src/contexts/EventsContext.jsx
+++ frontend/src/contexts/EventsContext.jsx
@@ -3,11 +3,13 @@
 import { createContext, useState, useEffect } from "react";
 import { getAllEvents } from "../services/eventServices";
 import { useUserContext } from "../contexts";
+import { sampleEventResponse } from "../services/eventsDataServices";
 
 export const EventsContext = createContext({});
 
 export default function EventsContextProvider({ children }) {
   const [allEvents, setAllEvents] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
   const [eventsLoading, setEventsLoading] = useState(false);
   const [eventsLoaded, setEventsLoaded] = useState(false);
   const { currentUser } = useUserContext();
@@ -18,7 +20,8 @@ export default function EventsContextProvider({ children }) {
       try {
         setEventsLoading(true);
         const id = currentUser?.userinfo?.userID;
-        const res = (await getAllEvents(id)).data;
+        // const res = (await getAllEvents(id)).data;
+        const res = sampleEventResponse;
         const data = res?.data?.events?.map((event) => {
           const copyOfEvent = { ...event };
           copyOfEvent.start = new Date(event.start_time);
@@ -27,7 +30,9 @@ export default function EventsContextProvider({ children }) {
 
           return copyOfEvent;
         });
+        const getPaginationDetails = res?.data?.pagination?.totalPages;
         setAllEvents(data);
+        setTotalPages(getPaginationDetails);
         setEventsLoading(false);
         setEventsLoaded(true);
       } catch (error) {
@@ -49,6 +54,8 @@ export default function EventsContextProvider({ children }) {
           setEventsLoading,
           eventsLoaded,
           setEventsLoaded,
+          totalPages,
+          setTotalPages,
         }}
       >
         {children}
--- frontend/src/pages/EventsPage/EventsPage.jsx
+++ frontend/src/pages/EventsPage/EventsPage.jsx
@@ -1,29 +1,88 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import { useEventsContext } from "../../contexts/index";
+import { useEventsContext, useUserContext } from "../../contexts/index";
 import { formatDate } from "../../helpers/formatDate";
 import EventCard from "./EventCard/EventCard";
-import { allEventsData } from "../../services/eventsDataServices";
+import {
+  allEventsData,
+  sampleEventResponse,
+} from "../../services/eventsDataServices";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { getEventsForPage } from "../../services/eventServices";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 const EventsPage = () => {
-  const { allEvents } = useEventsContext();
+  const { eventsLoading, totalPages } = useEventsContext();
+  const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [eventsToShowForPage, setEventsToShowForPage] = useState([]);
+  const [eventsForPageLoading, setEventsForPageLoading] = useState(true);
+  const { currentUser } = useUserContext();
+
+  useEffect(() => {
+    const page = searchParams.get("page");
+    if (page) {
+      setCurrentPage(parseInt(page));
+      const id = currentUser?.userinfo?.userID;
+      const eventsForPage = async () => {
+        // const events = (await getEventsForPage(id, page)).data;
+        const events = sampleEventResponse?.data?.events;
+        console.log(events);
+        setEventsToShowForPage(events);
+        setEventsForPageLoading(false);
+      };
+
+      eventsForPage();
+    }
+  }, [searchParams]);
+
+  const handlePageChange = (page) => {
+    if (page < 1 || page > totalPages) return;
+    setCurrentPage(page);
+    setSearchParams({ page: page });
+    navigate(`/dowellproctoring/events?page=${page}`);
+  };
 
   // console.log(allEventsData);
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.title}>My Events</h1>
-      <div className={styles.main__content}>
-        {allEvents.map((event) => (
-          <EventCard
-            key={event._id}
-            eventName={event.name}
-            startTime={formatDate(event.start_time)}
-            endTime={formatDate(event.close_date)}
-            participants={event.participants}
-          />
-        ))}
-      </div>
+      {eventsLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <div className={styles.main__content}>
+            {eventsToShowForPage.map((event) => (
+              <EventCard
+                key={event._id}
+                eventName={event.name}
+                startTime={formatDate(event.start_time)}
+                endTime={formatDate(event.close_date)}
+                participants={event.participants}
+              />
+            ))}
+          </div>
+          <div>
+            <button onClick={() => handlePageChange(currentPage - 1)}>
+              Prev
+            </button>
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <button
+                key={page}
+                disabled={currentPage === page}
+                onClick={() => handlePageChange(page)}
+              >
+                {page}
+              </button>
+            ))}
+            <button onClick={() => handlePageChange(currentPage + 1)}>
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </section>
   );
 };
