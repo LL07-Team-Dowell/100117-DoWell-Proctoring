101a5ecf00aebebde7f536479c5569dff06f4a9d
ParthChavda <76583713+ParthChavda@users.noreply.github.com>
2024-06-21 17:45:55 +0530

    Revert "Merge branch 'main' into parth-proctoring"
    This reverts commit b36c53893c427d237f36f124e2d9518d63863a26, reversing
    changes made to 686d7b6b72624d3385eed252460b85a59c3173f2.

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const AuthController = require("../controller/authController");
-
-const router = express.Router();
-
-router.get("/auth", AuthController.getAuthUrl);
-router.get("/oauth2callback", AuthController.oauth2Callback);
-
-module.exports = router;
