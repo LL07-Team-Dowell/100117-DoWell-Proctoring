9fc9e94fc48482b953c40ee015e71b3eb75c3fc9
ParthChavda <76583713+ParthChavda@users.noreply.github.com>
2024-07-07 17:45:09 +0530

    Update eventController.js

--- backend/controller/eventController.js
+++ backend/controller/eventController.js
@@ -42,10 +42,12 @@ class EventController {
     }
   }
 
+  
   // Method to get all events
   static async getAllEvents(req, res) {
     try {
       const { user_id, page = 1 } = req.query;
+      console.log(`hiiii: ${user_id}`)
       const limit = 20;
       const skip = (page - 1) * limit;
 
@@ -57,27 +59,27 @@ class EventController {
       // Prepare the aggregation pipeline
       const pipeline = [
         {
-          $match: matchStage,
+          $match: matchStage
         },
         {
           $lookup: {
-            from: "participants",
+            from: "participants", 
             localField: "_id", // The field from the "events" collection
             foreignField: "event_id", // The field from the "participants" collection
-            as: "active_participants",
-          },
+            as: "active_participants" 
+          }
         },
         {
           $sort: {
-            createdAt: -1, // Sort by createdAt field in descending order
-          },
+            createdAt: -1 // Sort by createdAt field in descending order
+          }
         },
         {
-          $skip: skip,
+          $skip: skip
         },
         {
-          $limit: limit,
-        },
+          $limit: limit
+        }
       ];
 
       const events = await Event.aggregate(pipeline);
@@ -86,33 +88,29 @@ class EventController {
       const nextPage = page < totalPages ? parseInt(page) + 1 : null;
       const prevPage = page > 1 ? parseInt(page) - 1 : null;
 
-      return res.status(200).json(
-        generateDefaultResponseObject({
-          success: true,
-          message: "Successfully fetched all events with participants",
-          data: {
-            pagination: {
-              totalEventsCount,
-              totalPages,
-              currentPage: parseInt(page),
-              nextPage,
-              prevPage,
-            },
-            events,
+      return res.status(200).json(generateDefaultResponseObject({
+        success: true,
+        message: 'Successfully fetched all events with participants',
+        data: {
+          pagination: {
+            totalEventsCount,
+            totalPages,
+            currentPage: parseInt(page),
+            nextPage,
+            prevPage
           },
-          error: null,
-        })
-      );
+          events
+        },
+        error: null,
+      }));
     } catch (error) {
       // Handle errors and send the error response
-      return res.status(500).json(
-        generateDefaultResponseObject({
-          success: false,
-          message: error.message,
-          data: null,
-          error: error.message,
-        })
-      );
+      return res.status(500).json(generateDefaultResponseObject({
+        success: false,
+        message: error.message,
+        data: null,
+        error: error.message,
+      }));
     }
   }
 
@@ -177,21 +175,21 @@ class EventController {
         {
           $match: {
             _id: new mongoose.Types.ObjectId(id),
-          },
+          }
         },
         {
           $lookup: {
-            from: "participants",
+            from: "participants", 
             localField: "_id", // The field from the "events" collection
             foreignField: "event_id", // The field from the "participants" collection
-            as: "active_participants",
-          },
+            as: "active_participants" 
+          }
         },
       ];
 
       const matchingEvent = await Event.aggregate(pipeline);
       const event = matchingEvent?.length > 0 ? matchingEvent[0] : null;
-
+      
       if (!event) {
         return res.status(404).json(
           generateDefaultResponseObject({
@@ -254,6 +252,125 @@ class EventController {
           message: error.message,
           data: null,
           error: null,
+        })
+      );
+    }
+  }
+
+  static async getEventReport(req, res) {
+    try {
+      const { id } = req.params;
+  
+      const pipeline = [
+        {
+          $match: {
+            _id: new mongoose.Types.ObjectId(id),
+          }
+        },
+        {
+          $lookup: {
+            from: "participants", 
+            localField: "_id", // The field from the "events" collection
+            foreignField: "event_id", // The field from the "participants" collection
+            as: "active_participants" 
+          }
+        },
+      ];
+  
+      const matchingEvent = await Event.aggregate(pipeline);
+      const event = matchingEvent?.length > 0 ? matchingEvent[0] : null;
+  
+      if (!event) {
+        return res.status(404).json(
+          generateDefaultResponseObject({
+            success: false,
+            message: "Event not found",
+            data: null,
+            error: null,
+          })
+        );
+      }
+  
+      const activeParticipants = Array.isArray(event.active_participants) ? event.active_participants : [];
+  
+      // Define your behavior-based criteria here
+      const goodParticipants = activeParticipants.filter(p => {
+        return p.hours_spent_in_event > 1 && !p.deleted;
+      });
+  
+      const badParticipants = activeParticipants.filter(p => {
+        return p.hours_spent_in_event <= 1 || p.deleted;
+      });
+      
+  
+      // Calculate average time spent
+      const totalHoursSpent = activeParticipants.reduce((sum, participant) => sum + (participant.hours_spent_in_event || 0), 0);
+      const averageTimeSpent = activeParticipants.length > 0 ? (totalHoursSpent / activeParticipants.length) : 0;
+      
+  
+      const report = {
+        proctors: {
+          total: 1,
+          ids: [event.user_id]
+        },
+        active_participants: {
+          total: activeParticipants.length,
+          ids: activeParticipants.map(participant => ({
+            id: participant._id,
+            time_joined: participant.createdAt,
+            time_registered: participant.updatedAt,
+            location: {
+              lat: participant.user_lat,
+              lon: participant.user_lon
+            }
+          })),
+          good: {
+            total: goodParticipants.length,
+            ids: goodParticipants.map(p => ({
+              id: p._id,
+              time_joined: p.createdAt,
+              time_registered: p.updatedAt,
+              location: {
+                lat: p.user_lat,
+                lon: p.user_lon
+              }
+            }))
+          },
+          bad: {
+            total: badParticipants.length,
+            ids: badParticipants.map(p => ({
+              id: p._id,
+              time_joined: p.createdAt,
+              time_registered: p.updatedAt,
+              location: {
+                lat: p.user_lat,
+                lon: p.user_lon
+              }
+            }))
+          }
+        },
+        duration: event.duration_in_hours + " hours",
+        start_time: event.start_time,
+        end_time: event.close_date,
+        locations: {}, 
+        average_time_spent: averageTimeSpent 
+      };
+  
+      return res.status(200).json(
+        generateDefaultResponseObject({
+          success: true,
+          message: "Successfully fetched event report",
+          data: report,
+          error: null,
+        })
+      );
+    } catch (error) {
+      return res.status(500).json(
+        generateDefaultResponseObject({
+          success: false,
+          message: error.message,
+          data: null,
+          error: error.message,
         })
       );
     }
